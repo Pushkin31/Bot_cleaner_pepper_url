df350e1fd9ecd033e82780d2e1a50e57ab85d84d
Alexander <35974828+Pushkin31@users.noreply.github.com>
2024-01-20 17:41:05 +0300

    update bot
    Добавил сохранение счётчика для всех пользователей с сохранением в sqlite3

--- Bot_cleaner_pepper_url.py
+++ Bot_cleaner_pepper_url.py
@@ -1,19 +1,32 @@
-#Bot_cleaner_pepper_url - это бот для конвертации ссылки pepper, которую получаем из "поделиться" в приложении.
-#https://github.com/Pushkin31/Bot_cleaner_pepper_url
+# Bot_cleaner_pepper_url - это бот для конвертации ссылки pepper, которую получаем из "поделиться" в приложении.
+# https://github.com/Pushkin31/Bot_cleaner_pepper_url
 
 import re
-import json
+import sqlite3
 import atexit
-import types
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram import executor
 
-TOKEN = 'YOUR_TOKEN'  # Заменить 'YOUR_TOKEN' на токен бота
-TARGET_USER_ID = 123456789  # ID Пользователя, от кого считаем кол-во ссылок
+# Заменить 'YOUR_TOKEN' на токен бота
+TOKEN = 'YOUR_TOKEN'
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
+
+# Создаем подключение к базе данных SQLite
+conn = sqlite3.connect('counters.db')
+cursor = conn.cursor()
+
+# Создаем таблицу, если она еще не существует
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS counters (
+        user_id INTEGER PRIMARY KEY,
+        count INTEGER
+    )
+''')
+conn.commit()
+
 
 def remove_path_from_url(url):
     cleaned_url = url.replace('/share-deal-from-app', '')
@@ -22,22 +35,6 @@ def remove_path_from_url(url):
 # Паттерн для поиска ссылок вида https://www.pepper.ru/share-deal-from-app/*
 url_pattern = re.compile(r'https://www\.pepper\.ru/share-deal-from-app/\d+')
 
-# Счетчик для конкретного пользователя
-user_counter = 0
-
-# Загрузка счетчика из файла
-try:
-    with open('counter.json', 'w') as f:
-        json.dump(user_counter, f)
-except Exception as e:
-    print(f"Ошибка при сохранении файла: {e}")
-
-def save_counter():
-    # Сохранение счетчика в файл
-    with open('counter.json', 'w') as f:
-        json.dump(user_counter, f)
-
-atexit.register(save_counter)
 
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
@@ -55,21 +52,47 @@ async def help_command(message: types.Message):
 async def about_bot(message: types.Message):
     await help_command(message)
 
+
+
 @dp.message_handler(lambda message: message.text and url_pattern.search(message.text))
 async def process_url(message: types.Message):
     original_url = message.text
     cleaned_url = remove_path_from_url(original_url)
 
-# Обработка счетчика только для указанного ID
-    if message.from_user.id == TARGET_USER_ID:
-        global user_counter
-        user_counter += 1
-        await message.reply(f"{cleaned_url}\n\nЭто уже {user_counter} неправильная ссылка с 29.12.2023", parse_mode=types.ParseMode.MARKDOWN)
+    # Обработка счетчика
+    user_id = message.from_user.id
+
+    # Получаем текущее значение счетчика из базы данных
+    cursor.execute('SELECT count FROM counters WHERE user_id=?', (user_id,))
+    result = cursor.fetchone()
+
+    if result:
+        count = result[0]
     else:
-        await message.reply(cleaned_url, parse_mode=types.ParseMode.MARKDOWN)
+        count = 0
+
+    # Увеличиваем значение счетчика на единицу
+    count += 1
+
+    # Сохраняем или обновляем значение счетчика в базе данных
+    cursor.execute('INSERT OR REPLACE INTO counters (user_id, count) VALUES (?, ?)', (user_id, count))
+    conn.commit()
+
+    # Вывод сообщения с учетом счетчика
+    await message.reply(
+        f"{cleaned_url}\n\nЭто уже {count} неправильная ссылка с 29.12.2023",
+        parse_mode=types.ParseMode.MARKDOWN)
 
     # Удаляем исходное сообщение пользователя
     await bot.delete_message(message.chat.id, message.message_id)
+
+
+# Закрываем подключение к базе данных при завершении программы
+def on_exit():
+    conn.close()
+
+
+atexit.register(on_exit)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
